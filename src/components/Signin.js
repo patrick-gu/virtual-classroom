fe239e9a65991ea955f049e1c7e2effdea112e69
Patrick Gu <patrick@patrickgu.ca>
2022-05-21 15:11:50 -0700

    Logging in now works correctly

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,71 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useUnauthenticated } from "../utils/auth";
 import "./Signup.css";
 
 const Signin = () => {
-    const setToken = useUnauthenticated();
-    return (
-        <main className="form-signin">
-            <form>
-                <h1 className="h3 mb-3 fw-normal heading signupContainer">
-                    <i className="fa-solid fa-screen-users"></i>Sign In
-                </h1>
-                <p>Get access to premium features</p>
+  const setToken = useUnauthenticated();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const { success, error, token } = await response.json();
+    if (success) {
+      setToken(token);
+      navigate("/profile");
+    } else {
+      alert(error);
+    }
+  };
+  return (
+    <main className="form-signin" onSubmit={onSubmit}>
+      <form>
+        <h1 className="h3 mb-3 fw-normal heading signupContainer">
+          <i className="fa-solid fa-screen-users"></i>Sign In
+        </h1>
+        <p>Get access to premium features</p>
 
-                <div className="form-floating">
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        name="email"
-                        placeholder="name@example.com"
-                    />
-                    <label for="email">Email address</label>
-                </div>
+        <div className="form-floating">
+          <input
+            type="username"
+            className="form-control"
+            id="username"
+            name="username"
+            placeholder="name@example.com"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label for="username">Username</label>
+        </div>
 
-                <div className="form-floating">
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        placeholder="Password"
-                    />
-                    <label for="password">Password</label>
-                </div>
-
-                <div className="dropdown mb-3 mt-3">
-                    <button
-                        className="btn btn-secondary dropdown-toggle"
-                        type="button"
-                        id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        Role
-                    </button>
-                    <ul
-                        className="dropdown-menu "
-                        aria-labelledby="dropdownMenuButton1"
-                    >
-                        <li>
-                            <a className="dropdown-item" href="#">
-                                Teacher
-                            </a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">
-                                Student
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <button className="w-100 btn btn-lg btn-primary" type="submit">
-                    <img className="icon" src="lock.svg" alt="" /> Sign in
-                </button>
-                <p className="mt-5 mb-3 text-muted">&copy; 2022</p>
-            </form>
-        </main>
-    );
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label for="password">Password</label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          <img className="icon" src="lock.svg" alt="" /> Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2022</p>
+      </form>
+    </main>
+  );
 };
 
 export default Signin;
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,41 +1,43 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const TOKEN_KEY = "VIRTUAL_CLASSROOM_TOKEN";
 
 export function useTokenState() {
-    return [
-        localStorage.getItem(TOKEN_KEY),
-        (token) => {
-            if (token) {
-                localStorage.setItem(TOKEN_KEY, token);
-            } else {
-                localStorage.removeItem(TOKEN_KEY);
-            }
-        },
-    ];
+  const [state, setState] = useState(() => localStorage.getItem(TOKEN_KEY));
+  return [
+    state,
+    (token) => {
+      if (token) {
+        localStorage.setItem(TOKEN_KEY, token);
+      } else {
+        localStorage.removeItem(TOKEN_KEY);
+      }
+      setState(token);
+    },
+  ];
 }
 
 export const AuthContext = createContext();
 
 export function useUnauthenticated() {
-    const [token, setToken] = useContext(AuthContext);
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (token) {
-            navigate("/classes");
-        }
-    });
-    return setToken;
+  const [token, setToken] = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate("/classes");
+    }
+  }, [token, navigate]);
+  return setToken;
 }
 
 export function useAuthenticated() {
-    const [token, setToken] = useContext(AuthContext);
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (!token) {
-            navigate("/login");
-        }
-    });
-    return [token, setToken];
+  const [token, setToken] = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+  return [token, setToken];
 }
