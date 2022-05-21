c1131c6b6a9990c6700538713f8e8b33ca24f7e5
Patrick Gu <patrick@patrickgu.ca>
2022-05-21 10:10:01 -0700

    Work in progress server

--- server/server.js
+++ server/server.js
@@ -1,37 +1,123 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
+const Fastify = require("fastify");
 const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const api = express.Router();
-
-const jsonParser = bodyParser.json();
-
-api.post("/auth/signup", jsonParser, async (req, res) => {
-    const { username, password, role } = req.body;
-    if (
-        typeof username !== "string" ||
-        !/^[a-zA-Z0-9\-_]{3,30}$/.test(username)
-    ) {
-        res.status(400).json({ error: "Invalid username" });
-        return;
-    }
-    if (typeof password !== "string" || !(10 <= password.length)) {
-        res.status(400).json({ error: "Invalid password" });
-        return;
-    }
-    if (role !== "student" && role !== "teacher") {
-        res.status(400).json({ error: "Invalid role" });
-        return;
-    }
-    res.send(`hello ${username}`);
+const fastify = Fastify({
+    logger: true,
 });
 
-const app = express();
-app.use("/api/v1", api);
-app.listen(8080);
-console.log("running server at localhost:8080");
+fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+});
+
+fastify.post(
+    "/api/v1/auth/register",
+    {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    username: { type: "string" },
+                    password: { type: "string" },
+                    role: { type: "string" },
+                },
+            },
+        },
+    },
+    async (request, reply) => {
+        let { username, password, role } = request.body;
+        if (
+            typeof username !== "string" ||
+            !/^[a-zA-Z0-9\-_]{3,30}$/.test(username)
+        ) {
+            reply.status(400);
+            return { error: "Invalid username" };
+        }
+        if (typeof password !== "string" || !(10 <= password.length)) {
+            reply.status(400);
+            return { error: "Invalid password" };
+        }
+        if (role === "student") {
+            role = "Student";
+        } else if (role === "teacher") {
+            role = "Teacher";
+        } else {
+            reply.status(400);
+            return { error: "Invalid role" };
+        }
+        const existing = await prisma.user.count({
+            where: { username },
+        });
+        if (existing) {
+            reply.status(409);
+            return { error: "Username already exists" };
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const { id } = await prisma.user.create({
+            data: {
+                username,
+                passwordHash,
+                role,
+            },
+            select: {
+                id: true,
+            },
+        });
+        return {
+            id,
+        };
+    }
+);
+
+fastify.post(
+    "/api/v1/auth/login",
+    {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    username: { type: "string" },
+                    password: { type: "string" },
+                },
+            },
+        },
+    },
+    async (request, reply) => {
+        const { username, password } = request.body;
+        const user = prisma.user.findUnique({
+            where: { username },
+            select: { id: true, passwordHash: true },
+        });
+        if (!user) {
+            reply.status(404);
+            return { error: "Username not found" };
+        }
+        const { id, passwordHash } = user;
+        const matches = bcrypt.compare(password, passwordHash);
+        if (!matches) {
+            reply.status(403);
+            return { error: "Wrong password" };
+        }
+    }
+);
+
+fastify.setErrorHandler(function (error, request, reply) {
+    fastify.log.error(error);
+    console.error(error);
+    reply.status(500).send({ error: "an error occured" });
+});
+
+const start = async () => {
+    try {
+        await fastify.listen(8080);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
