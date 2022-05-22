cd8d4aa8dbc95aba42301f4201b701dadff2fcde
Shekhar Dangi <arpitt682@gmail.com>
2022-05-22 16:58:32 +0530

    chat update

--- src/components/Class.js
+++ src/components/Class.js
@@ -141,10 +141,10 @@ export default function Class() {
       <div className="chat">
         <h2>Chat</h2>
 
-        <div className="chat__body">
+        <div className="chat__body ">
           {messages.map((message) => (
-            <div className="message_text" key={message.id}>
-              <p>{message.text}</p>
+            <div className={`message_text ${userId === message.userId ? "" : "message__reciever"}`}key={message.id}>
+              <p >{message.text}</p>
               <p>
                 <strong>
                   {users[message.userId]?.username ?? "Unknown"} -{" "}
