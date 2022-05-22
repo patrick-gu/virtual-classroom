fa8300938a7b0111c5f03e24e03d1c606b645d46
Shekhar Dangi <arpitt682@gmail.com>
2022-05-22 11:29:43 +0530

    added create classroom

--- server/server.js
+++ server/server.js
@@ -165,12 +165,20 @@ fastify.post(
         properties: {
           authorization: { type: "string" },
         },
-        required: ["authorization"],
       },
+      body: {
+        type: "object",
+        properties: {
+          className: { type: "string" },
+        },
+      },
+      required: ["authorization"],
     },
   },
-  async (req, reply) => {
-    const userId = verifySignedToken(req.headers.authorization);
+  async (request, reply) => {
+    const { classroomName } = request.body;
+    
+    const userId = verifySignedToken(request.headers.authorization);
     let code = "";
     const chars = "abcdefghijklmnopqrstuvwxyz".split("");
     for (let i = 0; i < 8; i++) {
@@ -178,7 +186,7 @@ fastify.post(
     }
     const { id: classroomId, name } = await prisma.classroom.create({
       data: {
-        name: "Unnamed Classroom",
+        name: classroomName,
         code,
         open: true,
       },
